fdb743369b462ac3f0f34546134ac0ab75b489b5
antoinecaron <antoinecaron.simplon@gmail.com>
2017-11-24 11:30:44 +0100

    exo_progress

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('have fun !');
+console.log([first_name, last_name, city]);
+
+var object = {}
+
+object ['first_name'] = $([type = ][nom = ]: )
+object ['last_name'] = $
+object ['city'] = $
